12a53e70ef94f269cb2ee86987a005a627fc8263
m7mdGNo <m7mdelganainy2001@gmail.com>
2023-08-22 16:17:17 -0700

    add aws deploy files

--- build.sh
+++
@@ -1,8 +0,0 @@
-echo "Building the project..."
-python3 -m pip install -r requirements.txt
-
-echo "Make Migration..."
-python3 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
