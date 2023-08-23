ac69983401186a9de074f4e49d8bd32dc096cc71
m7mdGNo <m7mdelganainy2001@gmail.com>
2023-08-22 19:18:07 -0700

    fix celery

--- cephalometric/settings.py
+++ cephalometric/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS", cast=list)
+# ALLOWED_HOSTS = env("ALLOWED_HOSTS", cast=list)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
