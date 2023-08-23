a3763759deb67764e6f9c71cdf8547354804cdf2
m7mdGNo <m7mdelganainy2001@gmail.com>
2023-08-22 19:20:57 -0700

    fix deploy

--- cephalometric/settings.py
+++ cephalometric/settings.py
@@ -78,7 +78,12 @@ WSGI_APPLICATION = 'cephalometric.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {"default": env.db()}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
